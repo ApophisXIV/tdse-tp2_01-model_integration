a5eed1d48365e166e8d6453e4b9ea92a8160e675
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2025-06-04 15:32:07 -0300

    [REFACTOR] Initialize task_system_dta with explicit fields

--- app/src/task_system.c
+++ app/src/task_system.c
@@ -41,146 +41,139 @@
 #include "main.h"
 
 /* Demo includes. */
-#include "logger.h"
 #include "dwt.h"
+#include "logger.h"
 
 /* Application & Tasks includes. */
-#include "board.h"
 #include "app.h"
-#include "task_system_attribute.h"
-#include "task_system_interface.h"
+#include "board.h"
 #include "task_actuator_attribute.h"
 #include "task_actuator_interface.h"
+#include "task_system_attribute.h"
+#include "task_system_interface.h"
 
 /********************** macros and definitions *******************************/
-#define G_TASK_SYS_CNT_INI			0ul
-#define G_TASK_SYS_TICK_CNT_INI		0ul
+#define G_TASK_SYS_CNT_INI      0ul
+#define G_TASK_SYS_TICK_CNT_INI 0ul
 
-#define DEL_SYS_XX_MIN				0ul
-#define DEL_SYS_XX_MED				50ul
-#define DEL_SYS_XX_MAX				500ul
+#define DEL_SYS_XX_MIN 0ul
+#define DEL_SYS_XX_MED 50ul
+#define DEL_SYS_XX_MAX 500ul
 
 /********************** internal data declaration ****************************/
-task_system_dta_t task_system_dta =
-	{DEL_SYS_XX_MIN, ST_SYS_XX_IDLE, EV_SYS_XX_IDLE, false};
+task_system_dta_t task_system_dta = {
+    .tick  = DEL_SYS_XX_MIN,
+    .state = ST_SYS_XX_IDLE,
+    .event = EV_SYS_XX_IDLE,
+    .flag  = false,
+};
 
-#define SYSTEM_DTA_QTY	(sizeof(task_system_dta)/sizeof(task_system_dta_t))
+#define SYSTEM_DTA_QTY (sizeof(task_system_dta) / sizeof(task_system_dta_t))
 
 /********************** internal functions declaration ***********************/
 
 /********************** internal data definition *****************************/
-const char *p_task_system 		= "Task System (System Statechart)";
-const char *p_task_system_ 		= "Non-Blocking & Update By Time Code";
+const char *p_task_system  = "Task System (System Statechart)";
+const char *p_task_system_ = "Non-Blocking & Update By Time Code";
 
 /********************** external data declaration ****************************/
 uint32_t g_task_system_cnt;
 volatile uint32_t g_task_system_tick_cnt;
 
 /********************** external functions definition ************************/
-void task_system_init(void *parameters)
-{
-	task_system_dta_t 	*p_task_system_dta;
-	task_system_st_t	state;
-	task_system_ev_t	event;
-	bool b_event;
+void task_system_init(void *parameters) {
+    task_system_dta_t *p_task_system_dta;
+    task_system_st_t state;
+    task_system_ev_t event;
+    bool b_event;
 
-	/* Print out: Task Initialized */
-	LOGGER_LOG("  %s is running - %s\r\n", GET_NAME(task_system_init), p_task_system);
-	LOGGER_LOG("  %s is a %s\r\n", GET_NAME(task_system), p_task_system_);
+    /* Print out: Task Initialized */
+    LOGGER_LOG("  %s is running - %s\r\n", GET_NAME(task_system_init), p_task_system);
+    LOGGER_LOG("  %s is a %s\r\n", GET_NAME(task_system), p_task_system_);
 
-	g_task_system_cnt = G_TASK_SYS_CNT_INI;
+    g_task_system_cnt = G_TASK_SYS_CNT_INI;
 
-	/* Print out: Task execution counter */
-	LOGGER_LOG("   %s = %lu\r\n", GET_NAME(g_task_system_cnt), g_task_system_cnt);
+    /* Print out: Task execution counter */
+    LOGGER_LOG("   %s = %lu\r\n", GET_NAME(g_task_system_cnt), g_task_system_cnt);
 
-	init_queue_event_task_system();
+    init_queue_event_task_system();
 
-	/* Update Task Actuator Configuration & Data Pointer */
-	p_task_system_dta = &task_system_dta;
+    /* Update Task Actuator Configuration & Data Pointer */
+    p_task_system_dta = &task_system_dta;
 
-	/* Print out: Task execution FSM */
-	state = p_task_system_dta->state;
-	LOGGER_LOG("   %s = %lu", GET_NAME(state), (uint32_t)state);
+    /* Print out: Task execution FSM */
+    state = p_task_system_dta->state;
+    LOGGER_LOG("   %s = %lu", GET_NAME(state), (uint32_t)state);
 
-	event = p_task_system_dta->event;
-	LOGGER_LOG("   %s = %lu", GET_NAME(event), (uint32_t)event);
+    event = p_task_system_dta->event;
+    LOGGER_LOG("   %s = %lu", GET_NAME(event), (uint32_t)event);
 
-	b_event = p_task_system_dta->flag;
-	LOGGER_LOG("   %s = %s\r\n", GET_NAME(b_event), (b_event ? "true" : "false"));
+    b_event = p_task_system_dta->flag;
+    LOGGER_LOG("   %s = %s\r\n", GET_NAME(b_event), (b_event ? "true" : "false"));
 
-	g_task_system_tick_cnt = G_TASK_SYS_TICK_CNT_INI;
+    g_task_system_tick_cnt = G_TASK_SYS_TICK_CNT_INI;
 }
 
-void task_system_update(void *parameters)
-{
-	task_system_dta_t *p_task_system_dta;
-	bool b_time_update_required = false;
+void task_system_update(void *parameters) {
+    task_system_dta_t *p_task_system_dta;
+    bool b_time_update_required = false;
 
-	/* Update Task System Counter */
-	g_task_system_cnt++;
+    /* Update Task System Counter */
+    g_task_system_cnt++;
 
-	/* Protect shared resource (g_task_system_tick) */
-	__asm("CPSID i");	/* disable interrupts*/
-    if (G_TASK_SYS_TICK_CNT_INI < g_task_system_tick_cnt)
-    {
-    	g_task_system_tick_cnt--;
-    	b_time_update_required = true;
+    /* Protect shared resource (g_task_system_tick) */
+    __asm("CPSID i"); /* disable interrupts*/
+    if (G_TASK_SYS_TICK_CNT_INI < g_task_system_tick_cnt) {
+        g_task_system_tick_cnt--;
+        b_time_update_required = true;
     }
-    __asm("CPSIE i");	/* enable interrupts*/
+    __asm("CPSIE i"); /* enable interrupts*/
 
-    while (b_time_update_required)
-    {
-		/* Protect shared resource (g_task_system_tick) */
-		__asm("CPSID i");	/* disable interrupts*/
-		if (G_TASK_SYS_TICK_CNT_INI < g_task_system_tick_cnt)
-		{
-			g_task_system_tick_cnt--;
-			b_time_update_required = true;
-		}
-		else
-		{
-			b_time_update_required = false;
-		}
-		__asm("CPSIE i");	/* enable interrupts*/
+    while (b_time_update_required) {
+        /* Protect shared resource (g_task_system_tick) */
+        __asm("CPSID i"); /* disable interrupts*/
+        if (G_TASK_SYS_TICK_CNT_INI < g_task_system_tick_cnt) {
+            g_task_system_tick_cnt--;
+            b_time_update_required = true;
+        } else {
+            b_time_update_required = false;
+        }
+        __asm("CPSIE i"); /* enable interrupts*/
 
-    	/* Update Task System Data Pointer */
-		p_task_system_dta = &task_system_dta;
+        /* Update Task System Data Pointer */
+        p_task_system_dta = &task_system_dta;
 
-		if (true == any_event_task_system())
-		{
-			p_task_system_dta->flag = true;
-			p_task_system_dta->event = get_event_task_system();
-		}
+        if (true == any_event_task_system()) {
+            p_task_system_dta->flag  = true;
+            p_task_system_dta->event = get_event_task_system();
+        }
 
-		switch (p_task_system_dta->state)
-		{
-			case ST_SYS_XX_IDLE:
+        switch (p_task_system_dta->state) {
+        case ST_SYS_XX_IDLE:
 
-				if ((true == p_task_system_dta->flag) && (EV_SYS_XX_ACTIVE == p_task_system_dta->event))
-				{
-					p_task_system_dta->flag = false;
-					put_event_task_actuator(EV_LED_XX_ON, ID_LED_A);
-					p_task_system_dta->state = ST_SYS_XX_ACTIVE;
-				}
+            if ((true == p_task_system_dta->flag) && (EV_SYS_XX_ACTIVE == p_task_system_dta->event)) {
+                p_task_system_dta->flag = false;
+                put_event_task_actuator(EV_LED_XX_ON, ID_LED_A);
+                p_task_system_dta->state = ST_SYS_XX_ACTIVE;
+            }
 
-				break;
+            break;
 
-			case ST_SYS_XX_ACTIVE:
+        case ST_SYS_XX_ACTIVE:
 
-				if ((true == p_task_system_dta->flag) && (EV_SYS_XX_IDLE == p_task_system_dta->event))
-				{
-					p_task_system_dta->flag = false;
-					put_event_task_actuator(EV_LED_XX_OFF, ID_LED_A);
-					p_task_system_dta->state = ST_SYS_XX_IDLE;
-				}
+            if ((true == p_task_system_dta->flag) && (EV_SYS_XX_IDLE == p_task_system_dta->event)) {
+                p_task_system_dta->flag = false;
+                put_event_task_actuator(EV_LED_XX_OFF, ID_LED_A);
+                p_task_system_dta->state = ST_SYS_XX_IDLE;
+            }
 
-				break;
+            break;
 
-			default:
+        default:
 
-				break;
-		}
-	}
+            break;
+        }
+    }
 }
 
 /********************** end of file ******************************************/
